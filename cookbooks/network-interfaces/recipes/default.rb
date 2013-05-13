7838f1d1e1fd7e3880268b846c18b0ad73fa36bf
Marin Orlic <marin.orlic@fer.hr>
2013-05-13 22:33:04 +0200

    Testing

--- cookbooks/network-interfaces/recipes/default.rb
+++ cookbooks/network-interfaces/recipes/default.rb
@@ -2,7 +2,17 @@
 # Cookbook Name:: network-interfaces
 # Recipe:: default
 #
-# Copyright 2013, YOUR_COMPANY_NAME
+# Copyright 2013, FER
 #
 # All rights reserved - Do Not Redistribute
 #
+
+template "/etc/network/interfaces-ipv6-chef" do
+  source "interfaces-ipv6-chef.erb"
+  owner "root"
+  group "root"
+  mode "0644"
+  variables(
+  	:interfaces => node[:interface_list]
+  )
+end
